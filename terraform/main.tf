34e37c3ca5e3f81464023388f6d5c279a410bf4f
data-sandbox <99933422+data-sandbox@users.noreply.github.com>
2025-05-25 18:07:27 -0600

    Init

---
+++ terraform/main.tf
@@ -0,0 +1,37 @@
+resource "azurerm_resource_group" "functions_rg" {
+  location = var.resource_group_location
+  name     = var.resource_group_name
+}
+
+resource "azurerm_storage_account" "storage_account" {
+  name                     = var.storage_account_name
+  resource_group_name      = azurerm_resource_group.functions_rg.name
+  location                 = azurerm_resource_group.functions_rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_service_plan" "service_plan" {
+  name                = var.service_plan_name
+  resource_group_name = azurerm_resource_group.functions_rg.name
+  location            = azurerm_resource_group.functions_rg.location
+  os_type             = "Linux"
+  sku_name            = "B1"
+}
+
+resource "azurerm_linux_function_app" "functions_demo" {
+  name                = var.function_app_name
+  resource_group_name = azurerm_resource_group.functions_rg.name
+  location            = azurerm_resource_group.functions_rg.location
+
+  storage_account_name       = azurerm_storage_account.storage_account.name
+  storage_account_access_key = azurerm_storage_account.storage_account.primary_access_key
+  service_plan_id            = azurerm_service_plan.service_plan.id
+
+  site_config {
+    application_stack {
+      python_version = 3.12
+    }
+  }
+
+}
