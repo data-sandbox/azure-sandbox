1f91bce39e2c88f1f0068711d19dedbeed55b68f
data-sandbox <99933422+data-sandbox@users.noreply.github.com>
2025-05-29 22:05:44 -0600

    Init bare bones fastapi

---
+++ app_service_demo/main.py
@@ -0,0 +1,47 @@
+# https://github.com/Azure-Samples/msdocs-python-fastapi-webapp-quickstart
+
+from fastapi import FastAPI, Form, Request, status
+from fastapi.responses import HTMLResponse, FileResponse, RedirectResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+import uvicorn
+
+
+app = FastAPI()
+app.mount("/static", StaticFiles(directory="app_service_demo/static"), name="static")
+templates = Jinja2Templates(directory="app_service_demo/templates")
+
+
+@app.get("/", response_class=HTMLResponse)
+async def index(request: Request):
+    print("Request for index page received")  # noqa T20
+    return templates.TemplateResponse("index.html", {"request": request})
+
+
+@app.get("/favicon.ico")
+async def favicon():
+    file_name = "favicon.ico"
+    file_path = "./static/" + file_name
+    return FileResponse(
+        path=file_path, headers={"mimetype": "image/vnd.microsoft.icon"}
+    )
+
+
+@app.post("/hello", response_class=HTMLResponse)
+async def hello(request: Request, name: str = Form(...)):
+    if name:
+        print("Request for hello page received with name=%s" % name)  # noqa T20
+        return templates.TemplateResponse(
+            "hello.html", {"request": request, "name": name}
+        )
+    else:
+        print(  # noqa T20
+            "Request for hello page received with no name or blank name -- redirecting"
+        )
+        return RedirectResponse(
+            request.url_for("index"), status_code=status.HTTP_302_FOUND
+        )
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=8000)
